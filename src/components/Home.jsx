5688d16e72422cf3e68924f733ca922c7e2739a2
Stanley <27.stanleylch@gmail.com>
2023-10-24 08:40:21 +0100

    5-links-to-individual-articles-by-id

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,22 +8,30 @@ import "../styles/Header.css"
 
 export default function fetchArticleList() {
     const [articles, setArticles] = useState([])
+    const [isLoading, setLoading] = useState(true)
     const articleListUrl = "https://ncnews1.onrender.com/api/articles"
     
 
     useEffect(() => {
         axios.get(articleListUrl)
         .then((response) => {
+
             const mostRecentThreeArticles = response.data.articles.sort((a, b) => a.created_at - b.created_at)
-            setArticles(mostRecentThreeArticles.slice(0,3)) 
+            setArticles(mostRecentThreeArticles.slice(0,3))
+            setLoading(false)
         })
         .catch((err) =>{
             console.log(err)
+            setLoading(false)
         })
     }, [])
 
 
-    return (
+    return isLoading ? (
+        <section className="loadingState">
+            <h2>currently loading articles</h2>
+        </section>
+    ):(
         <div className="homeContainer">
             <section className="homeText">
                 <h3>
@@ -31,8 +39,8 @@ export default function fetchArticleList() {
                 </h3>
 
                 <ul className="articleList">
-                    {articles.map((article, index) => {
-                        return <li key = {"article" + index} className="articleSection">
+                    {articles.map((article) => {
+                        return <li key = {"article" + article.article_id} className="articleSection">
                             <img src={article.article_img_url}></img>
                             <br></br>
                             Title: {article.title}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,10 +7,6 @@ import "../styles/NavBar.css"
 export default function NavBar(){
     return(
         <div className="navContainer">
-            <h3>
-                This is the navbar
-            </h3>
-
             <nav className="navItems">
                 <Link to="/">Home</Link>
                 <Link to="/articles">Article List</Link>
